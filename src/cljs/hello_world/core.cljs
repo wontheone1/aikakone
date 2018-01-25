0071f960aad88b1f459b83db41f096ed74842494
WON SEOB SEO <won.seob.seo@zalando.fi>
2018-01-25 22:50:25 +0200

    Use websocket to propagate hello world text movement

--- src/cljs/hello_world/core.cljs
+++ src/cljs/hello_world/core.cljs
@@ -2,10 +2,53 @@
   (:require [goog.events :as events]
             [nightlight.repl-server]
             [play-cljs.core :as p]
+            [taoensso.sente  :as sente :refer (cb-success?)]
             ))
+
+(enable-console-print!)
+
+(defn get-chsk-url
+  "Connect to a configured server instead of the page host"
+  [protocol chsk-host chsk-path type]
+  (let [protocol (case type :ajax protocol
+                            :ws   (if (= protocol "https:") "wss:" "ws:"))]
+    (str protocol "//" "localhost:2222" chsk-path)))
+
+(with-redefs
+  [sente/get-chsk-url get-chsk-url]
+  (let [{:keys [ch-recv send-fn]}
+        (sente/make-channel-socket! "/chsk"
+                                    {:type :auto})]
+    (def ch-chsk ch-recv)
+    (def chsk-send! send-fn)))
 
 (defonce game (p/create-game (.-innerWidth js/window) (.-innerHeight js/window)))
 (defonce state (atom {}))
+
+(defmulti event-msg-handler :id)
+
+(defmethod event-msg-handler :default [{:keys [event]}]
+  (println "Unhandled event: " event))
+
+(defmethod event-msg-handler :chsk/state [{:keys [?data]}]
+  (if (= ?data {:first-open? true})
+    (println "Channel socket successfully established!")
+    (println "Channel socket state change:" ?data)))
+
+(defmethod event-msg-handler :chsk/recv [{:keys [?data]}]
+  (let [position (second ?data)]
+    (swap! state assoc :text-x (:x position) :text-y (:y position))))
+
+(defn send-uid []
+  (chsk-send! [:aikakone/uid (:uid @state)]))
+
+(defmethod event-msg-handler :chsk/handshake [{:keys [?data]}]
+  (let [[?uid ?csrf-token ?handshake-data] ?data]
+    (println "Handshake:" ?data)
+    (swap! state assoc :uid ?uid)
+    (send-uid)))
+
+(sente/start-chsk-router! ch-chsk event-msg-handler)
 
 (def main-screen
   (reify p/Screen
@@ -22,7 +65,11 @@
 
 (events/listen js/window "mousemove"
   (fn [event]
-    (swap! state assoc :text-x (.-clientX event) :text-y (.-clientY event))))
+    (swap! state assoc :text-x (.-clientX event) :text-y (.-clientY event))
+    (let [{:keys [text-x text-y]} @state]
+      (chsk-send!
+        [:aikakone/mouse-moved {:x text-x :y text-y}]
+        3000))))
 
 (events/listen js/window "resize"
   (fn [event]
