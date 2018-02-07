b8979fd69516800cd319d2f9d74b96c7a802befa
WON SEOB SEO <pghoou@gmail.com>
2018-02-07 22:25:50 +0200

    Use phaser to load a image
    https://phaser.io/tutorials/making-your-first-phaser-game/index
    
    https://phaser.io/tutorials/making-your-first-phaser-game/part2

--- src/cljs/hello_world/core.cljs
+++ src/cljs/hello_world/core.cljs
@@ -6,4 +6,23 @@
 
 (enable-console-print!)
 
+(declare game)
+
+(defn- preload []
+  (.image (.-load game) "puzzle" "images/puzzle-image.jpg"))
+
+(defn- create []
+  (.sprite (.-add game) 0 0 "puzzle"))
+
+(defn- update [])
+
+(defonce game (js/Phaser.Game.
+                (.-innerWidth js/window)
+                (.-innerHeight js/window)
+                js/Phaser.Auto
+                "" ; id of the DOM element to insert canvas. As we've left it blank it will simply be appended to body.
+                (clj->js {:preload preload :create create :update update})))
+
+(defonce state (atom {}))
+
 (web-sck/start-router state)
