4e20689112f984f42c706b9558cf430c717ba4ef
WON SEOB SEO <pghoou@gmail.com>
2018-02-19 00:40:44 +0200

    Add control buttons on top left of the screen

--- src/cljs/hello_world/core.cljs
+++ src/cljs/hello_world/core.cljs
@@ -20,6 +20,12 @@
   (/ @puzzle-width col-num))
 (defn- piece-height [puzzle-height]
   (/ @puzzle-height row-num))
+(def button-sprite-sheet-width (atom nil))
+(def button-sprite-sheet-height (atom nil))
+(defn- button-width [sheet-width]
+  (/ @sheet-width 3))
+(defn- button-height [sheet-height]
+  (/ @sheet-height 2))
 
 (def game (atom nil))
 
@@ -30,14 +36,23 @@
     "images/puzzle-image.jpg"
     (piece-width puzzle-width)
     (piece-height puzzle-height)
-    (* row-num col-num)))
+    (* row-num col-num))
+  (.spritesheet
+    (.-load @game)
+    "flip-buttons"
+    "images/control-buttons.png"
+    (button-width button-sprite-sheet-width)
+    (button-height button-sprite-sheet-height)
+    6))
 
 (defn- create []
   (let [game-object-factory (.-add @game)
         left-margin (left-margin window-width)
         top-margin (top-margin window-height)
         piece-width (piece-width puzzle-width)
-        piece-height (piece-height puzzle-height)]
+        piece-height (piece-height puzzle-height)
+        button-width (button-width button-sprite-sheet-width)
+        button-height (button-height button-sprite-sheet-height)]
     (doseq [row (range row-num)
             col (range col-num)
             :let [frame-id (+ (* col-num row) col)
@@ -48,7 +63,15 @@
         x-pos
         y-pos
         "puzzle"
-        frame-id))))
+        frame-id))
+    (doseq [i (range 6)]
+      (println button-width :* button-height)
+      (.sprite
+        game-object-factory
+        (* i button-width)
+        (* i button-height)
+        "flip-buttons"
+        i))))
 
 (defn- update [])
 
@@ -69,10 +92,20 @@
 (web-sck/start-router state)
 
 ; this is the game program's entry point
-(let [puzzle-img (js/Image.)]
+(let [puzzle-img (js/Image.)
+      buttons-img (js/Image.)]
   ; finding out size of image. https://stackoverflow.com/a/626505/5802173
   ; image loading is done asynchronously. The way to start the game after image is loaded is
-  ; we start the game in `onload` callback of the image.
+  ; we start the game in `onload` callback of the image. After loading buttons-img first,
+  ; start loading puzzle image then start the game.
+  (set!
+    (.-onload buttons-img)
+    (clj->js
+      (fn []
+        (reset! button-sprite-sheet-width (.-width buttons-img))
+        (reset! button-sprite-sheet-height (.-height buttons-img))
+        (println "buttons image loaded")
+        (set! (.-src puzzle-img) "images/puzzle-image.jpg"))))
   (set!
     (.-onload puzzle-img)
     (clj->js
@@ -81,5 +114,5 @@
         (reset! puzzle-height (.-height puzzle-img))
         (println "Puzzle image loaded")
         (start-game!))))                                    ; start game after loading image
-  (set! (.-src puzzle-img) "images/puzzle-image.jpg")
-  (println "loading puzzle image"))
+  (set! (.-src buttons-img) "images/control-buttons.png")
+  (println "loading images"))
