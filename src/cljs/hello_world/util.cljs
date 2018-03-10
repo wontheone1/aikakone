8bc4a1b15f88896ff42bb5fd7ec5f0564dae5bf4
WON SEOB SEO <pghoou@gmail.com>
2018-03-10 19:33:13 +0200

    Do not synchronise puzzle board when not playing

--- src/cljs/hello_world/util.cljs
+++ src/cljs/hello_world/util.cljs
@@ -36,6 +36,11 @@
 (defn- top-margin []
   (/ (- (.-innerHeight js/window) (:puzzle-width-height @game-state)) 4))
 
+(defn- currently-playing-game? []
+  (let [dereffed-game-state @game-state]
+    (and (not (empty? (:sprites dereffed-game-state)))
+         (nil? (:stage-clear-text dereffed-game-state)))))
+
 (defn show-congrat-message-and-play-button-when-puzzle-is-complete! []
   (when (and (every? #(= non-flipped-state (val %)) (:sprites-state @game-state))
              (not (:stage-clear-text @game-state)))
@@ -53,28 +58,29 @@
                        :align "center"})))))
 
 (defn- synchronize-puzzle-board [sprites-state]
-  (println "synchronizing")
-  (let [derefed-state @game-state
-        piece-x-scale (:piece-x-scale derefed-state)
-        piece-y-scale (:piece-y-scale derefed-state)
-        sprites (:sprites derefed-state)]
-    (doseq [[[col row] sprite-flipped-state] sprites-state]
-      (let [piece-scale (.-scale (sprites [col row]))]
-        (if (= non-flipped-state sprite-flipped-state)
-          (do
-            (swap!
-              game-state
-              assoc-in
-              [:sprites-state [col row]]
-              non-flipped-state)
-            (.setTo piece-scale piece-x-scale piece-y-scale))
-          (do
-            (swap!
-              game-state
-              assoc-in
-              [:sprites-state [col row]]
-              flipped-state)
-            (.setTo piece-scale 0 0)))))))
+  (when (currently-playing-game?)
+    (println "synchronizing")
+    (let [derefed-state @game-state
+          piece-x-scale (:piece-x-scale derefed-state)
+          piece-y-scale (:piece-y-scale derefed-state)
+          sprites (:sprites derefed-state)]
+      (doseq [[[col row] sprite-flipped-state] sprites-state]
+        (let [piece-scale (.-scale (sprites [col row]))]
+          (if (= non-flipped-state sprite-flipped-state)
+            (do
+              (swap!
+                game-state
+                assoc-in
+                [:sprites-state [col row]]
+                non-flipped-state)
+              (.setTo piece-scale piece-x-scale piece-y-scale))
+            (do
+              (swap!
+                game-state
+                assoc-in
+                [:sprites-state [col row]]
+                flipped-state)
+              (.setTo piece-scale 0 0))))))))
 
 (defn destroy-stage-clear-text! []
   (when-let [stage-clear-text (:stage-clear-text @game-state)]
