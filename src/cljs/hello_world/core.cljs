e5faceeb079542720942fd33e31d034eb669ed54
WON SEOB SEO <pghoou@gmail.com>
2018-03-29 00:13:59 +0300

    Enable console print! in core

--- src/cljs/hello_world/core.cljs
+++ src/cljs/hello_world/core.cljs
@@ -13,6 +13,8 @@
             [reagent.core :as r]
             ))
 
+(enable-console-print!)
+
 (defn go-back-to-game-button []
   [ui/mui-theme-provider
    {:muiTheme (get-mui-theme {:palette {:textColor (color :blue200)}})}
