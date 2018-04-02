86d48cd40f16a09b999abf70c38f6e54d83bc7c2
WON SEOB SEO <pghoou@gmail.com>
2018-04-02 18:11:25 +0300

    Use ref instead of atom and use transaction

--- src/clj/hello_world/core.clj
+++ src/clj/hello_world/core.clj
@@ -20,13 +20,13 @@
   (def chsk-send!                    send-fn)
   (def connected-uids                connected-uids))
 
-(def sprites-state (atom nil))
+(def sprites-state (ref nil))
 
-(def ranking (atom []))
+(def ranking (ref []))
 
-(def game-start-time (atom nil))
+(def game-start-time (ref nil))
 
-(def sending-time-future (atom nil))
+(def sending-time-future (ref nil))
 
 (defn- convert-to-millis [seconds nanos]
   (+ (* 1000 seconds) (/ nanos 1000000)))
@@ -50,30 +50,30 @@
 (defn- handle-message! [{:keys [id client-id ?data]}]
   (case id
     :aikakone/sprites-state
-    (do
-      (reset! sprites-state ?data)
+    (dosync
+      (ref-set sprites-state ?data)
       (send-data-to-all-except-message-sender client-id :aikakone/sprites-state ?data))
 
     :aikakone/game-start
     (chsk-send! client-id [:aikakone/game-start @sprites-state])
 
     :aikakone/start-timer
-    (do
-      (reset! game-start-time (t/local-date-time))
-      (reset! sending-time-future (start-sending-current-playtime!)))
+    (dosync
+      (ref-set game-start-time (t/local-date-time))
+      (ref-set sending-time-future (start-sending-current-playtime!)))
 
     :aikakone/puzzle-complete!
-    (do
-      (reset! game-start-time nil)
-      (reset! sprites-state nil)
-      (swap! ranking (fn [ranking]
+    (dosync
+      (ref-set game-start-time nil)
+      (ref-set sprites-state nil)
+      (alter ranking (fn [ranking]
                        (sort (conj ranking ?data))))
       (send-data-to-all-except-message-sender client-id :aikakone/sprites-state {}))
 
     :aikakone/reset
-    (do
-      (reset! game-start-time nil)
-      (reset! sprites-state nil)
+    (dosync
+      (ref-set game-start-time nil)
+      (ref-set sprites-state nil)
       (send-data-to-all-except-message-sender client-id :aikakone/reset nil))
 
     nil))
